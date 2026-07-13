95bbc75848071a3aca4716f3b1fd44e3189e8a72
Agustín <start.agencia.contacto@gmail.com>
2026-07-12 22:32:52 -0300

    Agregar vite.config.js y workflow de despliegue a GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './' // Using relative paths makes the build portable to both local previews, CrazyGames, and GitHub Pages!
+});
